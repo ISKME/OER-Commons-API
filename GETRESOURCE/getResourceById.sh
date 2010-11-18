5ca19680cb33b4b5e0f814238e33fcdfadc220b7
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2010-11-18 14:04:11 -0500

    Simple variable interpolation

--- GETRESOURCE/getResourceById.sh
+++ GETRESOURCE/getResourceById.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# A simple script for fetching a resource.  No auth required.
+# A simple script for fetching a resource.  No auth required.  Returns 
+# a JSON-encoded result set.
 
-curl http://staging.oercommons.org/api/getResource?id=6431
+# curl http://www.oercommons.org/api/getResource?id=6431
+curl http://www.oercommons.org/api/getResource?id=$1
