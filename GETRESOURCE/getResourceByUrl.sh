2da16c2de55077aecbc0eca70802651c758558a2
Greg DeKoenigsberg <greg.dekoenigsberg@gmail.com>
2010-11-18 17:30:45 -0500

    modify to accept argument

--- GETRESOURCE/getResourceByUrl.sh
+++ GETRESOURCE/getResourceByUrl.sh
@@ -2,4 +2,5 @@
 
 # A simple script for fetching a resource.  No auth required.
 
-curl http://staging.oercommons.org/api/getResource?url=https://open.umich.edu/education/si/si680-winter2008
+#curl http://staging.oercommons.org/api/getResource?url=https://open.umich.edu/education/si/si680-winter2008
+curl http://staging.oercommons.org/api/getResource?url=$1
